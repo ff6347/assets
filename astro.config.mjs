a9691f27bc6f95c3a88d3b78671dd1e0aa81f938
ff6347 <hash@ff6347.email>
2024-09-23 09:38:32 +0200

    fix: Set origin for overview on client

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+	site: import.meta.env.PROD ? "https://assets.qawsed.site" : "http://localhost:4321",
+});
